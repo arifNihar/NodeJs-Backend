e0b52d8e6b00601f6f498316beb2e07e62903144
arifNihar <arif.khan.nihar.90@gmail.com>
2024-01-20 04:37:17 +0600

    post route and controller api complete

--- Monolithic/agamir-threads/server.js
+++ Monolithic/agamir-threads/server.js
@@ -3,6 +3,8 @@ import dotenv from "dotenv";
 import connectDB from "./db/connectDB.js";
 import cookieParser from "cookie-parser";
 import userRoutes from "./routes/userRoutes.js";
+import postRoutes from "./routes/postRoutes.js";
+import { v2 as cloudinary } from "cloudinary";
 
 dotenv.config();
 
@@ -10,13 +12,20 @@ connectDB();
 const app = express();
 const PORT = process.env.PORT || 5000;
 
+cloudinary.config({
+  cloud_name: process.env.CLOUDINARY_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
+});
+
 //Middleware
-app.use(express.json()); // To parse JSON data in the req.body
+app.use(express.json({ limit: "50mb" })); // To parse JSON data in the req.body
 app.use(express.urlencoded({ extended: true })); // To parse form data in the req.body
 app.use(cookieParser());
 
 //Routes
 app.use("/api/users", userRoutes);
+app.use("/api/posts", postRoutes);
 
 app.listen(PORT, () =>
   console.log(`Server started at htpp://localhost:${PORT}`)
